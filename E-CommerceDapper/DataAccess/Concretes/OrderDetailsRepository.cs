fdf435347ae49d60bd8c4c998900169a002bd913
camalzadeelvin <camalzadeelvin@STHQ012D-01>
2022-12-18 17:22:12 +0400

    Add project files.

---
+++ E-CommerceDapper/DataAccess/Concretes/OrderDetailsRepository.cs
@@ -0,0 +1,38 @@
+﻿using E_CommerceDapper.DataAccess.Abstractions;
+using E_CommerceDapper.DataAccess.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceDapper.DataAccess.Concretes
+{
+    public class OrderDetailsRepository : IOrderDetailsRepository
+    {
+        public void Add(OrderDetails data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderDetails Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<OrderDetails> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(OrderDetails data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
